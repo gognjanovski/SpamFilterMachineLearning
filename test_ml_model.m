299a538b0325849ede2d903bb9933f1217ee8c6e
Gavril Ognjanovski <g.ognjanovski@hotmail.com>
2018-11-08 22:23:57 +0100

    Add Test ML Model

---
+++ test_ml_model.m
@@ -0,0 +1,20 @@
+#TEST DATA
+
+N = dlmread('test-features.txt', ' ');
+test_matrix = sparse(N(:, 1), N(:, 2), N(:, 3));
+
+numTestDocs = size(test_matrix, 1);
+numTestTokens = size(test_matrix, 2);
+
+output = zeros(numTestDocs, 1);
+
+log_a = test_matrix*log(prob_token_spam') + log(prob_spam);
+log_b = test_matrix*log(prob_token_nonspam') + log(1-prob_spam);
+
+output = log_a > log_b;
+
+test_labels = dlmread('test-labels.txt', ' ');
+
+wrong_classification =  sum(xor(output, test_labels));
+
+error = wrong_classification/numTestDocs
